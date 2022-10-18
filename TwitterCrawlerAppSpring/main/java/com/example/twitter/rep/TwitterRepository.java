b8045c37274abac791f09c3b9ebd035ac90093fb
SakhaMe <71602813+sakharamgawas@users.noreply.github.com>
2022-10-18 15:23:01 +0530

    Delete TwitterCrawlerAppSpring/main/java/com/example/twitter directory

--- TwitterCrawlerAppSpring/main/java/com/example/twitter/rep/TwitterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.twitter.rep;
-
-import com.example.twitter.models.Tweets;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TwitterRepository extends MongoRepository<Tweets,Integer>{
-    
-}
